5829ea66d38ea3fcb135dd19964737e07a79248a
josesantos <jose.santos@educbank.com.br>
2026-01-29 11:02:53 -0300

    fix: Removendo logs e permissions

--- src/KarTime.Application.Contracts/Permissions/KarTimePermissionDefinitionProvider.cs
+++ src/KarTime.Application.Contracts/Permissions/KarTimePermissionDefinitionProvider.cs
@@ -11,9 +11,6 @@ public class KarTimePermissionDefinitionProvider : PermissionDefinitionProvider
     {
         var myGroup = context.AddGroup(KarTimePermissions.GroupName);
 
-        myGroup.AddPermission(KarTimePermissions.LapTime.Default);
-        myGroup.AddPermission(KarTimePermissions.LapTime.Post);
-        myGroup.AddPermission(KarTimePermissions.LapTime.Get);
         //Define your own permissions here. Example:
         //myGroup.AddPermission(KarTimePermissions.MyPermission1, L("Permission:MyPermission1"));
     }
--- src/KarTime.Application.Contracts/Permissions/KarTimePermissions.cs
+++ src/KarTime.Application.Contracts/Permissions/KarTimePermissions.cs
@@ -3,13 +3,6 @@ namespace KarTime.Permissions;
 public static class KarTimePermissions
 {
     public const string GroupName = "KarTime";
-
-    public struct LapTime
-    {
-        public const string Default = GroupName + ".LapTime";
-        public const string Get = Default + ".Get";
-        public const string Post = Default + ".Post";
-    }
     
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
