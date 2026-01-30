d178dfafeccbb8c5ddb2c5ddcf2e8fa897bdd6d0
josesantos <jose.santos@educbank.com.br>
2026-01-30 09:01:47 -0300

    fix: formataçã

--- src/KarTime.Application.Contracts/Permissions/KarTimePermissions.cs
+++ src/KarTime.Application.Contracts/Permissions/KarTimePermissions.cs
@@ -3,8 +3,8 @@ namespace KarTime.Permissions;
 public static class KarTimePermissions
 {
     public const string GroupName = "KarTime";
-    
-    
+
+
     
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
